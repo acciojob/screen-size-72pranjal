0143fbb29a5b8adbd409f1d016ffa7c99054d27f
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-19 09:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let div = document.querySelector('#sizeInfo')
+        let h1 = document.createElement('h1')
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+        h1.innerText = `Width: ${screenWidth} and Height: ${screenHeight}`
+        div.append(h1)
+        window.addEventListener('resize', ()=> {
+            const screenWidth = window.innerWidth;
+            const screenHeight = window.innerHeight;
+            h1.innerText = `Width: ${screenWidth} and Height: ${screenHeight}`
+        })
